5137f4dd1a9762f170bbf3431c994dd14865f789
theoroiroi <114597785+theoroiroi@users.noreply.github.com>
2022-11-20 11:19:13 +0100

    Commit7(final)

--- Assets/MyGame/Scripts/FingeruebungArray.cs
+++ Assets/MyGame/Scripts/FingeruebungArray.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class FingeruebungArray : MonoBehaviour
 {
@@ -10,7 +11,13 @@ public class FingeruebungArray : MonoBehaviour
     string[] array4 = new string[10];
     [SerializeField] GameObject[] arrayColoredImagesRed;
     string longstring;
-
+    int[] array5 = new int[48];
+    int[] array6 = new int[498];
+    int[] array7 = new int[498];
+    string[] array8 = new string[100];
+    int[] array9 = new int[1000];
+    int[] array11 = new int[1000];
+    int[] array12 = new int[1000];
 
     private void Start()
     {
@@ -27,10 +34,10 @@ public class FingeruebungArray : MonoBehaviour
         Debug.Log(array4[0]);
         Debug.Log(arrayColoredImagesRed[0].name);
 
-        Debug.Log(array1[array1.Length-1]);
-        Debug.Log(array2[array2.Length-1]);
-        Debug.Log(array3[array3.Length-1]);
-        Debug.Log(array4[array4.Length-1]);
+        Debug.Log(array1[array1.Length - 1]);
+        Debug.Log(array2[array2.Length - 1]);
+        Debug.Log(array3[array3.Length - 1]);
+        Debug.Log(array4[array4.Length - 1]);
         Debug.Log(arrayColoredImagesRed[arrayColoredImagesRed.Length - 1].name);
 
         for (int i = 0; i < array2.Length; i++)
@@ -44,19 +51,79 @@ public class FingeruebungArray : MonoBehaviour
         Debug.Log(longstring);
 
         array1[3] = 3;
-        Debug.Log(array1);
+        Debug.Log(array1[3]);
 
         array2[2] = 203;
-        Debug.Log(array2);
+        Debug.Log(array2[2]);
 
         array3[5] = 333;
-        Debug.Log(array3);
+        Debug.Log(array3[5]);
 
         array4[2] = "heute";
-        Debug.Log(array4);
+        Debug.Log(array4[2]);
 
         arrayColoredImagesRed[2].GetComponent<Image>().color = Color.green;
 
-    }
+        for (int i = 0; i < array5.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array5[i] = i + 22;
+            }
+        }
 
+        for (int i = 0; i < array6.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array6[i] = i + 2;
+            }
+        }
+
+        for (int i = 0; i < array7.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                array7[i] = i;
+            }
+        }
+
+        for (int i = 0; i < array8.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array8[i] = "a";
+            }
+            else if (i % 2 == 1)
+            {
+                array8[i] = "b";
+            }
+        }
+
+        for(int i = 0; i < array9.Length; i++)
+        {
+            array9[i] = Random.Range(0, 1000);
+        }
+        System.Array.Sort(array9);
+        for(int i = 0; i < array9.Length; i++)
+        {
+            Debug.Log(array9[i]);
+        }
+
+        int[] array10 = array5.Concat(array6).ToArray();
+
+        for(int i = 0; i < array11.Length; i++)
+        {
+            array11[i] = Random.Range(0, 1000);
+        }
+        System.Array.Sort(array11);
+        Debug.Log(array11[0] + " und " + array11[array11.Length - 1]);
+
+        for(int i = 0; i < array12.Length; i++)
+        {
+            array12[i] = Random.Range(0, 1000);
+        }
+        int randomNum = Random.Range(0, 1000);
+        Debug.Log(System.Array.IndexOf(array12, randomNum));
+    }
 }
